f27e32dff17d2dbabc9a1d362de1c01c46b8edf0
funghicat87 <funghicat87@gmail.com>
2024-08-20 17:38:45 +0800

    完成教學

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,22 @@
 import * as React from 'react'
 import { Link } from 'gatsby'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
+import Seo from '../components/seo'
+
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage
+        alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
+        src="../images/dog.webp"
+      />
     </Layout>
   )
 }
 
-export const Head = () => <title>Home Page</title>
+export const Head = () => <Seo title="Home Page" />
 
 export default IndexPage
